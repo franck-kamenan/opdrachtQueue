5c95825ef1751c32b0db897df46ebb00c976bdc6
Franck Kamenan <franck.kamenan@yahoo.com>
2020-09-22 12:24:02 +0200

    Queue LinkedList offer peek

---
+++ QueueApp.java
@@ -0,0 +1,18 @@
+package be.intecbrussel.opdrachten.opdrachtqueue;
+
+import java.util.*;
+
+public class QueueApp {
+
+    public static void main(String[] args) {
+
+        Queue<String> names = new LinkedList<>();
+        names.offer("Jean-Marie"); //adding an element
+        names.offer("Jean-Bart"); //adding an element
+        names.offer("Jean-Alexandre"); //adding an element
+
+        System.out.println(names.peek());
+        System.out.println(names.peek());
+        System.out.println(names.peek());
+    }
+}
