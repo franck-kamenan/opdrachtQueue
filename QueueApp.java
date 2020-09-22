e2685c95ad483cfc30711b77d449657afc46c5a5
Franck Kamenan <franck.kamenan@yahoo.com>
2020-09-22 12:37:34 +0200

    PriorityQueue

--- QueueApp.java
+++ QueueApp.java
@@ -6,12 +6,14 @@ public class QueueApp {
 
     public static void main(String[] args) {
 
-        Queue<String> names = new LinkedList<>();
+        Queue<String> names2 = new LinkedList<>();
+        Queue<String> names = new PriorityQueue<>();
         names.offer("Jean-Marie"); //adding an element
         names.offer("Jean-Bart"); //adding an element
         names.offer("Jean-Alexandre"); //adding an element
 
-        System.out.println(names.remove()); // Show the removed element.
+        System.out.println(names.peek());
+        System.out.println(names.poll());
         System.out.println(names.remove()); // Show the removed element.
         System.out.println(names.remove()); // Show the removed element.
     }
